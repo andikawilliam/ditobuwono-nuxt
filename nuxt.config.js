439aaa4a3418e498e4599a5dfc18cd9e0156bc48
vandercoder <andikawilliam@outlook.com>
2021-08-17 12:48:02 +0700

    Update title website

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'ditobuwono-nuxt',
+    title: 'Hanindito Buwono',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },

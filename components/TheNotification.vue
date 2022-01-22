783ebe12820751e58299f4a0f73b1d6b91d2db0c
vandercoder <andikawilliam@outlook.com>
2022-01-22 12:56:11 +0700

    Update notification styling

--- components/TheNotification.vue
+++ components/TheNotification.vue
@@ -5,68 +5,20 @@
     class="fixed w-full lg:w-2/5 px-6 mx-auto lg:right-0 bottom-2"
     v-show="notifState && notifText"
   >
-    <div class="py-2 px-1 sm:px-6 lg:px-10 bg-gray-dito rounded-xl">
+    <div
+      class="py-2 px-2 sm:px-4 border-2 bg-white border-gray-dito rounded-xl"
+    >
       <div class="flex items-center justify-between flex-wrap">
-        <div class="w-0 flex-1 flex items-center text-white text-xs md:text-sm">
-          <span class="hidden md:flex p-1 rounded-lg">
-            <!-- Heroicon name: outline/speakerphone -->
-            <svg
-              class="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z"
-              />
-            </svg>
-          </span>
-          <p class="ml-3 font-medium">
-            <span class="md:hidden">
+        <div
+          class="w-0 flex-1 flex items-center text-gray-dito text-xs md:text-sm"
+        >
+          <p class="ml-1 font-medium">
+            <span class="inline">
               <a :href="notifLink">
                 {{ this.notifText }}
               </a>
             </span>
-            <span class="hidden md:inline">
-              {{ this.notifText }}
-            </span>
           </p>
-        </div>
-        <div
-          class="
-            order-3
-            mt-2
-            flex-shrink-0
-            w-full
-            hidden
-            sm:order-2 sm:mt-0 sm:w-auto
-            md:inline
-          "
-        >
-          <a
-            :href="notifLink"
-            class="
-              flex
-              items-center
-              justify-center
-              px-4
-              py-2
-              border border-transparent
-              rounded-md
-              shadow-sm
-              text-sm
-              font-medium
-              bg-white
-              hover:bg-gray-50
-            "
-          >
-            Get it now
-          </a>
         </div>
         <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
           <button

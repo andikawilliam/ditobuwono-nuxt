23b1a182b1b60917836f0c571858fedf78c2f58e
hanindito <74034463+hanindito@users.noreply.github.com>
2023-01-20 13:28:27 +0700

    Update meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Hanindito Buwono is a filmmaker and writer based in Jakarta, Indonesia.' }
+      { name: 'description', content: 'Hanindito Buwono is a filmmaker and writer based in Jakarta, Indonesia. Completed his psychology study at Gadjah Mada University, currently Dito are interested in working and developing more in the creative and research fields.' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
